ca146df64d3412e2309d67b3ad4e4e02dac767e7
Gabulon <gabo.fajardo7@gmail.com>
2024-06-07 12:49:36 -0600

    Consultando las REST API en Vue.js

--- src/views/ClientesView.vue
+++ src/views/ClientesView.vue
@@ -1,6 +1,16 @@
 <script>
+import {onMounted}from 'vue'
+import axios from 'axios'
 import RouterLink from '../components/UI/RouterLink.vue';
 import Heading from '../components/UI/Heading.vue';
+
+onMounted(() =>{
+    axios.get('http://localhost:4000/clientes')
+    .then(({data}) => {
+        console.log(data)
+    })
+    .catch(error => console.log('Huboun error'))
+})
 
 defineProps({
         titulo: {
