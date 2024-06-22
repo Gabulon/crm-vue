571a0717af3e76b1c8ff3fc7fa096a30be94cd68
Gabulon <gabo.fajardo7@gmail.com>
2024-06-21 19:30:28 -0600

    Comenzando con la edición de Clientes y Routing Dinamico en Vue-Router

---
+++ src/views/EditarCliente.vue
@@ -0,0 +1,95 @@
+<script>
+import ClienteService from "../services/ClienteService";
+import { FormKit, componentSymbol } from "@formkit/vue";
+import { useRouter } from "vue-router";
+import RouterLink from "../components/UI/RouterLink.vue";
+import Heading from "../components/UI/Heading.vue";
+
+const router = useRouter()
+
+defineProps({
+  titulo: {
+    type: String
+  }
+})
+
+// const formData = reactive({
+//     nombre:'Juan'
+// })
+
+const handleSubmit = (data) => {
+ 
+}
+</script>
+
+<template>
+  <div>
+    <div class="flex justify-end">
+      <RouterLink to="listado-clientes"> Volver </RouterLink>
+    </div>
+    <Heading>{{ titulo }}</Heading>
+
+    <div class="mx-auto mt-10 bg-white shadow">
+      <div class="mx-auto md:w-2/3 py-20 px-6">
+        <FormKit type="form" 
+        submit-label="Agregar Cliente"
+        incomplete-message="No se pudo enviar, revisa los mensajes"
+        @submit="handleSubmit"
+        >
+          <FormKit
+            type="text"
+            label="Nombre"
+            name="nombre"
+            placeholder="Nombre de Cliente"
+            validation="required"
+            :validation-messages="{
+              required: 'El nombre del Cliente es Obligatorio',
+            }"
+          />
+          <FormKit
+            type="text"
+            label="Apellido"
+            name="apellido"
+            placeholder="Apellido de Cliente"
+            validation="required"
+            :validation-messages="{
+              required: 'El apellido del Cliente es Obligatorio',
+            }"
+          />
+          <FormKit
+            type="email"
+            label="Email"
+            name="email"
+            placeholder="Email de Cliente"
+            validation="required|email"
+            :validation-messages="{
+              required: 'El email del Cliente es Obligatorio',
+              email: 'Coloca un email valido',
+            }"
+          />
+          <FormKit
+            type="text"
+            label="Telefono"
+            name="telefono"
+            placeholder="Telefono: XXX-XXX-XXXX"
+            validation="*matches:/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/"
+            :validation-messages="{ matches: 'El formato no es valido' }"
+          />
+          <FormKit
+            type="text"
+            label="Empresa"
+            name="empresa"
+            placeholder="Empresa de Cliente"
+          />
+          <FormKit type="text" label="Puesto" placeholder="Puesto de Cliente"  name="puesto" />
+        </FormKit>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.formkit-wrapper {
+  max-width: 100%;
+}
+</style>
